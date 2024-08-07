bba92fd65d55d4df60ba9b9a5cad48c9ceb9308c
Mdkaifq <140621920+Mdkaifq@users.noreply.github.com>
2024-08-07 20:05:43 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -179,7 +179,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS=[
+STATICFILES_DIR=[
     BASE_DIR / 'static',
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
